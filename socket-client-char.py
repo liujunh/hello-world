2ea136dffb7b5115f5e55f987954e077b9a14462
liujunh <39983065+liujunh@users.noreply.github.com>
2018-06-08 13:38:17 +0800

    python套接字编程，客户端（TCP）

---
+++ socket-client-char.py
@@ -0,0 +1,28 @@
+# -*- coding=utf-8 -*-
+
+import socket
+import sys
+
+def socket_client():
+              try:
+                            #创建socket
+                            s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+                            #与服务器建立连接
+                            s.connect(('192.168.17.131',6666))
+              #检错
+              except socket.error as msg:
+                            print 'msg'
+                            sys.exit(1)
+              #接收服务端欢迎消息
+              print s.recv(1024)
+              #发送数据
+              while 1:
+                            data = raw_input('what do you want to send: ')
+                            s.send(data)
+                            print s.recv(1024)
+                            if data == 'exit':
+                                          break
+              s.close()
+
+if __name__ == '__main__':
+              socket_client()
